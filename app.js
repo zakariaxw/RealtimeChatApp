1f81d0fdda7c331ebeaf4f676fde059bd4034757
zakariaxw <zakariajr963@gmail.com>
2024-02-27 12:29:39 +0100

    setup files & depandecies

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+const app = require("express")();
+
+const http = require("http").Server(app);
 require("dotenv").config();
 
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/realtimeapp");
+
+
+
+
+app.get('/', (req, res)=>{
+
+
+    res.send('welcome');
+});
+
+
+
+
+
+http.listen(3000, function(){
+
+    console.log("server is runing");
+});
